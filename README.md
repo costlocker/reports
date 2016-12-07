6f0560fb447c58d1cb896bcfd8fdedb764d7f5d4
Zdeněk Drahoš <drahoszdenek@gmail.com>
2016-12-07 09:25:07 +0100

    Init command for generating report

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ```bash
 # crontab
-0 8 1 * * bin/console report --date "previous month" --host "new.costlocker.com:apiKey" --email "kamil@costlocker.com" 2>&1 >> report.log
+0 8 1 * * bin/console report --date "previous month" --host "https://new.costlocker.com|apiKey" --email "kamil@costlocker.com" 2>&1 >> report.log
 ```
---
+++ src/GenerateReportCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Costlocker\Reports;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GenerateReportCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('report')
+            ->addOption('date', 'd', InputOption::VALUE_REQUIRED, 'Select month', 'previous month')
+            ->addOption('host', 'a', InputOption::VALUE_REQUIRED, 'apiUrl|apiKey')
+            ->addOption('email', 'e', InputOption::VALUE_OPTIONAL, 'Report recipients');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $month = new \DateTime($input->getOption('date'));
+        list($apiHost, $apiKey) = explode('|', $input->getOption('host'));
+        $email = $input->getOption('email');
+
+        $output->writeln([
+            "<comment>Report</comment>",
+            "<info>Month:</info> {$month->format('Y-m')}",
+            "<info>API Url:</info> {$apiHost}",
+            "<info>API Key:</info> {$apiKey}",
+            "<info>E-mail Recipients:</info> {$email}",
+        ]);
+    }
+}
