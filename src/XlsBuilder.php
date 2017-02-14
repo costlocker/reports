bc4b26642bba78e737c87307e707f886248958ee
Zdeněk Drahoš <drahoszdenek@gmail.com>
2017-02-14 08:56:44 +0100

    Profitability - positions - cleanup xls builder

--- src/XlsBuilder.php
+++ src/XlsBuilder.php
@@ -12,29 +12,28 @@ use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 class XlsBuilder
 {
     private $worksheet;
-    private $rowId;
+    private $rowId = 1;
 
     public function __construct(Spreadsheet $spreadsheet, $title)
     {
         $this->worksheet = $spreadsheet->createSheet();
         $this->worksheet->setTitle($title);
-        $this->rowId = 1;
     }
 
-    public function addHeaders(array $headers, $hasUnit = true)
+    public function addHeaders(array $headers)
     {
-        $unitRowId = $hasUnit ? $this->rowId + 1 : $this->rowId;
+        $unitRowId = $this->rowId + 1;
         foreach ($headers as $index => $header) {
             $column = $this->indexToLetter($index);
             list($header, $backgroundColor, $unit) = $header + [0 => '', 1 => 'transparent', 2 => null];
-            if ($hasUnit && $unit) {
-                $this->worksheet->setCellValue("{$column}{$this->rowId}", $header);
-                $this->worksheet->setCellValue("{$column}{$unitRowId}", "[{$unit}]");
-            } elseif ($hasUnit) {
-                $this->worksheet->setCellValue("{$column}{$this->rowId}", $header);
-                $this->worksheet->mergeCells("{$column}{$this->rowId}:{$column}{$unitRowId}");
+            if ($unit) {
+                $this->worksheet
+                    ->setCellValue("{$column}{$this->rowId}", $header)
+                    ->setCellValue("{$column}{$unitRowId}", "[{$unit}]");
             } else {
-                $this->worksheet->setCellValue("{$column}{$this->rowId}", $header);
+                $this->worksheet
+                    ->setCellValue("{$column}{$this->rowId}", $header)
+                    ->mergeCells("{$column}{$this->rowId}:{$column}{$unitRowId}");
             }
             $this->worksheet->getStyle("{$column}{$this->rowId}:{$column}{$unitRowId}")->applyFromArray([
                 'fill' => [
@@ -47,12 +46,9 @@ class XlsBuilder
             $this->worksheet->getColumnDimension($column)->setAutoSize(true);
         }
         
-        $this->addStyle(0, count($headers), 'transparent', Alignment::HORIZONTAL_CENTER);
-        if ($hasUnit) {
-            $this->addStyle(0, count($headers), 'transparent', Alignment::HORIZONTAL_CENTER);
-        }
-
-        return $this;
+        return $this
+            ->addStyle(0, count($headers), 'transparent', Alignment::HORIZONTAL_CENTER)
+            ->addStyle(0, count($headers), 'transparent', Alignment::HORIZONTAL_CENTER);
     }
 
     public function addRow(array $data, $backgroundColor = null, $alignment = null)
@@ -62,17 +58,15 @@ class XlsBuilder
                 list($value, $format, $conditionals) = $value + [2 => []];
                 $coordinate = "{$this->indexToLetter($index)}{$this->rowId}";
                 $cell = $this->worksheet->setCellValue($coordinate, $value, true);
-                $cell->getStyle()->getNumberFormat()->setFormatCode($format);
-                if ($conditionals) {
-                    $cell->getStyle()->setConditionalStyles($conditionals);
-                }
+                $cell->getStyle()
+                    ->setConditionalStyles($conditionals)
+                    ->getNumberFormat()->setFormatCode($format);
             } else {
                 $this->worksheet->setCellValue("{$this->indexToLetter($index)}{$this->rowId}", $value);
             }
         }
         reset($data);
-        $this->addStyle(key($data), count($data), $backgroundColor, $alignment);
-        return $this;
+        return $this->addStyle(key($data), count($data), $backgroundColor, $alignment);
     }
 
     private function addStyle($firstColumnIndex, $columnsCount, $backgroundColor = null, $alignment = null)
@@ -109,6 +103,8 @@ class XlsBuilder
         $this->worksheet->getStyle("{$firstColumn}{$this->rowId}:{$lastColumn}{$this->rowId}")
             ->applyFromArray($styles);
         $this->rowId++;
+
+        return $this;
     }
 
     public function mergeCells($firstColumn, $lastColumn)
